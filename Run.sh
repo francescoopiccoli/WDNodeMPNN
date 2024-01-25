d7e31a9cbde00e61ac41fe74ae2310db0e257ea1
francescoopiccoli <xfrancescopiccolix@gmail.com>
2024-01-25 16:32:47 +0100

    refactor: refactoring

--- Run.sh
+++ Run.sh
@@ -1,7 +1,5 @@
-# run data.py once
-python Transform_Batch_Data.py
-cd ..
-
+# [TODO-F] Fix this file to run the supervised training for 10 iterations and save the results in the RUNS folder
+ 
 # Create Models Weights and Figures Directories and clear them
 mkdir Models_Weights
 rm -rf Models_Weights/*
@@ -18,17 +16,17 @@ echo "!!! - STARTING SUPERVISED RUNS - !!!"
 for i in {1..10}
 do 
     # make a directory for that run and a weights and figures directory to which to copy the results from the run
-    mkdir Run_$i
-    cd Run_$i
+    mkdir RUN_$i
+    cd RUN_$i
     mkdir Models_Weights
     mkdir Figures
 
     # Run the supervised train python file
-    cd ../../
+    cd ../../src
     echo "RUNNING ITERATION $i"
     echo "_____________________"
     echo "Starting Supervised"
-    python Supervised_train.py
+    python training.py
     
     # copy the model weights and figures to the folder in run
     cp -r Models_Weights/* RUNS/RUN_$i/Models_Weights
--- src/WDNodeMPNN.py
+++ src/WDNodeMPNN.py
@@ -6,21 +6,19 @@ from torch_geometric.nn import global_mean_pool
 
 # node-centered message passing
 class WDNodeMPNN(nn.Module):
-    class_name = "node-centered-mpnn"
+    class_name = "WDNodeMPNN"
 
     def __init__(
             self, 
             node_attr_dim, 
             edge_attr_dim,
             n_message_passing_layers=3,
-            hidden_dim=300,
-            dropout_rate=0.1, 
+            hidden_dim=300
         ):
         
         super().__init__()
         self.node_attr_dim = node_attr_dim
         self.edge_attr_dim = edge_attr_dim
-        self.dropout_rate = dropout_rate
         self.n_message_passing_layers = n_message_passing_layers
         self.message_passing_layers = nn.ModuleList()
         self.lin0 = nn.Linear(node_attr_dim + edge_attr_dim, hidden_dim)
@@ -52,11 +50,11 @@ class WDNodeMPNN(nn.Module):
     def forward(self, data):
         x, edge_index, edge_attr, edge_weight, node_weight = data.x, data.edge_index, data.edge_attr, data.edge_weight, data.node_weight    
         
-        incoming_edge_weighted_sum = torch.zeros(x.size()[0], edge_attr.size()[1])
+        incoming_edges_weighted_sum = torch.zeros(x.size()[0], edge_attr.size()[1])
         edge_index_reshaped = edge_index[1].view(-1, 1)
         # sum over the rows (edges), index is the target node (i want to sum all edges where the target node is the same), src = attributes weighted
-        incoming_edge_weighted_sum.scatter_add_(0, edge_index_reshaped.expand_as(edge_attr), edge_weight.view(-1, 1) * edge_attr)
-        concat_features = torch.cat([x, incoming_edge_weighted_sum], dim=1)
+        incoming_edges_weighted_sum.scatter_add_(0, edge_index_reshaped.expand_as(edge_attr), edge_weight.view(-1, 1) * edge_attr)
+        concat_features = torch.cat([x, incoming_edges_weighted_sum], dim=1)
       
         h0 = self.lin0(concat_features)
         h0 = F.relu(h0)
@@ -67,7 +65,7 @@ class WDNodeMPNN(nn.Module):
         # concatenate the node features with the embedding from the message passing layers
         h = self.final_message_passing_layer(torch.cat([h, x], dim=1), edge_index, edge_weight, h0)
 
-        # atom_weight to use stoichiometry
+        # node_weight to use stoichiometry
         graph_embedding = global_mean_pool(h * node_weight.view(-1, 1), data.batch)
 
         out = self.final_mlp(graph_embedding)
@@ -96,7 +94,7 @@ class MessagePassingLayer(MessagePassing):
         return self.propagate(edge_index, x=h_t, edge_weight=edge_weight, h0=h0)
 
     
-    # Constructs messages to node i ifor each edge (j,i) if flow="source_to_target"
+    # Constructs messages to node i for each edge (j,i) if flow="source_to_target"
     def message(self, x_i, edge_weight): 
         # x_i contains the node features of the source nodes for each edge. [num_edges, hidden_lin_dim]   
         # x_j contains the node features of the target nodes for each edge. [num_edges, hidden_lin_dim]
